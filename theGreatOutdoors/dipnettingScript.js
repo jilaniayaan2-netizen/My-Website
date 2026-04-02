2ec06b0ac4f94e9f3e53a33984319f7a44dcc479
rainystudios <121326358+rainystudios@users.noreply.github.com>
2026-04-02 12:46:52 -0400

    finished all the catches for river1, including info and completed game loop

--- theGreatOutdoors/dipnettingScript.js
+++ theGreatOutdoors/dipnettingScript.js
@@ -34,63 +34,75 @@ var river1Catch = document.getElementById("river1Catch");
 var river2Catch = document.getElementById("river2Catch");
 var river3Catch = document.getElementById("river3Catch");
 var pickCatch = 0;
-var numOfLeaves = 4; 
+var numOfLeaves = 4;
 var currentRiver = 0;
+var fishCaught = 0;
 
 if (mag1) {
   mag1.addEventListener('click', function () {
     currentRiver = 1;
     document.body.style.backgroundImage = "url('river1Background.png')";
     pickCatch = Math.floor(Math.random() * (100 - 1 + 1) + 1);
-    if (pickCatch === 1) {
-      river1Catch.style.width = 449 + "px"; 
+    if (pickCatch === 1 || pickCatch === 100) {
+      river1Catch.style.width = 449 + "px";
       river1Catch.style.height = 337 + "px";
       river1Catch.style.backgroundImage = "url('paintedhandMudbug.png')";
+      fishCaught = 1;
     }
     else if (2 <= pickCatch && pickCatch <= 4) {
-      river1Catch.style.width = 448 + "px"; 
+      river1Catch.style.width = 448 + "px";
       river1Catch.style.height = 336 + "px";
       river1Catch.style.backgroundImage = "url('calicoCrayfish.png')";
+      fishCaught = 2;
     }
     else if (5 <= pickCatch && pickCatch <= 9) {
-      river1Catch.style.width = 252 + "px"; 
+      river1Catch.style.width = 252 + "px";
       river1Catch.style.height = 336 + "px";
       river1Catch.style.backgroundImage = "url('bigWaterCrayfish.png')";
+      fishCaught = 3;
     }
     else if (10 <= pickCatch && pickCatch <= 14) {
-      river1Catch.style.width = 449 + "px"; 
-      river1Catch.style.height = 337 + "px";
+      river1Catch.style.width = 224.5 + "px";
+      river1Catch.style.height = 168.5 + "px";
       river1Catch.style.backgroundImage = "url('fatheadMinnow.png')";
+      fishCaught = 4;
     }
     else if (15 <= pickCatch && pickCatch <= 19) {
-      river1Catch.style.width = 506 + "px"; 
+      river1Catch.style.width = 506 + "px";
       river1Catch.style.height = 336 + "px";
       river1Catch.style.backgroundImage = "url('centralStoneroller.png')";
+      fishCaught = 5;
     }
     else if (20 <= pickCatch && pickCatch <= 29) {
-      river1Catch.style.width = 252 + "px"; 
+      river1Catch.style.width = 252 + "px";
       river1Catch.style.height = 336 + "px";
       river1Catch.style.backgroundImage = "url('virileCrayfish.png')";
+      fishCaught = 6;
     }
     else if (30 <= pickCatch && pickCatch <= 39) {
-      river1Catch.style.width = 252 + "px"; 
+      river1Catch.style.width = 252 + "px";
       river1Catch.style.height = 336 + "px";
       river1Catch.style.backgroundImage = "url('riverChub.png')";
+      fishCaught = 7;
     }
     else if (40 <= pickCatch && pickCatch <= 49) {
-      river1Catch.style.width = 693 + "px"; 
-      river1Catch.style.height = 337 + "px";
+      river1Catch.style.width = 346.5 + "px";
+      river1Catch.style.height = 168 + "px";
+      river1Catch.style.bottom = 40 + "%";
       river1Catch.style.backgroundImage = "url('goldenShiner.png')";
+      fishCaught = 8;
     }
     else if (50 <= pickCatch && pickCatch <= 69) {
-      river1Catch.style.width = 253 + "px"; 
+      river1Catch.style.width = 253 + "px";
       river1Catch.style.height = 336 + "px";
       river1Catch.style.backgroundImage = "url('northernClearwaterCrayfish.png')";
+      fishCaught = 9;
     }
     else {
-      river1Catch.style.width = 252 + "px"; 
+      river1Catch.style.width = 252 + "px";
       river1Catch.style.height = 336 + "px";
       river1Catch.style.backgroundImage = "url('commonShiner.png')";
+      fishCaught = 10;
     }
   });
 }
@@ -99,35 +111,65 @@ if (mag2) {
     currentRiver = 2;
     document.body.style.backgroundImage = "url('river2Background.png')";
     pickCatch = Math.floor(Math.random() * (100 - 1 + 1) + 1);
-    if(pickCatch === 1){
-      console.log("Redside Dace");
+    if (pickCatch === 1 || pickCatch === 100) {
+      river2Catch.style.width = 252.5 + "px";
+      river2Catch.style.height = 168.5 + "px";
+      river2Catch.style.backgroundImage = "url('redsideDace.png')";
+      fishCaught = 11;
     }
-    else if(2 <= pickCatch && pickCatch <= 4){
-      console.log("Northern Pearl Dace");
+    else if (2 <= pickCatch && pickCatch <= 4) {
+      river2Catch.style.width = 442.5 + "px";
+      river2Catch.style.height = 168 + "px";
+      river2Catch.style.backgroundImage = "url('northernPearlDace.png')";
+      fishCaught = 12;
     }
-    else if(5 <= pickCatch && pickCatch <= 9){
-      console.log("Common Log Perch");
+    else if (5 <= pickCatch && pickCatch <= 9) {
+      river2Catch.style.width = 315.5 + "px";
+      river2Catch.style.height = 168 + "px";
+      river2Catch.style.backgroundImage = "url('commonLogperch.png')";
+      fishCaught = 13;
     }
-    else if(10 <= pickCatch && pickCatch <= 14){
-      console.log("Greenside Darter");
+    else if (10 <= pickCatch && pickCatch <= 14) {
+      river2Catch.style.width = 299 + "px";
+      river2Catch.style.height = 168.5 + "px";
+      river2Catch.style.backgroundImage = "url('greensideDarter.png')";
+      fishCaught = 14;
     }
-    else if(15 <= pickCatch && pickCatch <= 20){
-      console.log("Iowa Darter");
+    else if (15 <= pickCatch && pickCatch <= 20) {
+      river2Catch.style.width = 253 + "px";
+      river2Catch.style.height = 168 + "px";
+      river2Catch.style.backgroundImage = "url('iowaDarter.png')";
+      fishCaught = 15;
     }
-    else if(21 <= pickCatch && pickCatch <= 30){
-      console.log("Rainbow Darter");
+    else if (21 <= pickCatch && pickCatch <= 30) {
+      river2Catch.style.width = 252 + "px";
+      river2Catch.style.height = 336 + "px";
+      river2Catch.style.backgroundImage = "url('rainbowDarter.png')";
+      fishCaught = 16;
     }
-    else if(31 <= pickCatch && pickCatch <= 40){
-      console.log("Johnny Darter");
+    else if (31 <= pickCatch && pickCatch <= 40) {
+      river2Catch.style.width = 252 + "px";
+      river2Catch.style.height = 336 + "px";
+      river2Catch.style.backgroundImage = "url('johnnyDarter.png')";
+      fishCaught = 17;
     }
-    else if(41 <= pickCatch && pickCatch <= 60){
-      console.log("Fantail Darter");
+    else if (41 <= pickCatch && pickCatch <= 60) {
+      river2Catch.style.width = 252 + "px";
+      river2Catch.style.height = 336 + "px";
+      river2Catch.style.backgroundImage = "url('fantailDarter.png')";
+      fishCaught = 18;
     }
-    else if(61 <= pickCatch && pickCatch <= 80){
-      console.log("Blacknose Dace");
+    else if (61 <= pickCatch && pickCatch <= 80) {
+      river2Catch.style.width = 252 + "px";
+      river2Catch.style.height = 336 + "px";
+      river2Catch.style.backgroundImage = "url('westernBlacknoseDace.png')";
+      fishCaught = 19;
     }
-    else{
-      console.log("Longnose Dace");
+    else {
+      river2Catch.style.width = 249.5 + "px";
+      river2Catch.style.height = 168.5 + "px";
+      river2Catch.style.backgroundImage = "url('longnoseDace.png')";
+      fishCaught = 20;
     }
   });
 }
@@ -136,37 +178,162 @@ if (mag3) {
     currentRiver = 3;
     document.body.style.backgroundImage = "url('river3Background.png')";
     pickCatch = Math.floor(Math.random() * (100 - 1 + 1) + 1);
-    if(pickCatch === 1){
-      console.log("Brindled Madtom");
+    if (pickCatch === 1 || pickCatch === 100) {
+      river3Catch.style.width = 299.5 + "px";
+      river3Catch.style.height = 168.5 + "px";
+      river3Catch.style.backgroundImage = "url('brindledMadtom.png')";
+      fishCaught = 21;
     }
-    else if(2 <= pickCatch && pickCatch <= 4){
-      console.log("Moltted Sculpin");
+    else if (2 <= pickCatch && pickCatch <= 4) {
+      river3Catch.style.width = 280.5 + "px";
+      river3Catch.style.height = 168 + "px";
+      river3Catch.style.backgroundImage = "url('mottledSculpin.png')";
+      fishCaught = 22;
     }
-    else if(5 <= pickCatch && pickCatch <= 7){
-      console.log("Brook Silverside");
+    else if (5 <= pickCatch && pickCatch <= 8) {
+      river3Catch.style.width = 288.5 + "px";
+      river3Catch.style.height = 84 + "px";
+      river3Catch.style.backgroundImage = "url('brookSilverside.png')";
+      fishCaught = 23;
     }
-    else if(8 <= pickCatch && pickCatch <= 11){
-      console.log("Brook Stickleback");
+    else if (9 <= pickCatch && pickCatch <= 15) {
+      river3Catch.style.width = 253 + "px";
+      river3Catch.style.height = 336 + "px";
+      river3Catch.style.backgroundImage = "url('brookStickleback.png')";
+      fishCaught = 24;
     }
-    else if(12 <= pickCatch && pickCatch <= 18){
-      console.log("Banded Killifish");
+    else if (16 <= pickCatch && pickCatch <= 22) {
+      river3Catch.style.width = 224.5 + "px";
+      river3Catch.style.height = 168.5 + "px";
+      river3Catch.style.backgroundImage = "url('bandedKillifish.png')";
+      fishCaught = 25;
     }
-    else if(19 <= pickCatch && pickCatch <= 28){
-      console.log("Central Mudminnow");
+    else if (23 <= pickCatch && pickCatch <= 32) {
+      river3Catch.style.width = 299.5 + "px";
+      river3Catch.style.height = 168 + "px";
+      river3Catch.style.backgroundImage = "url('centralMudminnow.png')";
+      fishCaught = 26;
     }
-    else if(29 <= pickCatch && pickCatch <= 40){
-      console.log("Slimey Sculpin");
+    else if (33 <= pickCatch && pickCatch <= 47) {
+      river3Catch.style.width = 253 + "px";
+      river3Catch.style.height = 168 + "px";
+      river3Catch.style.backgroundImage = "url('slimySculpin.png')";
+      fishCaught = 27;
     }
-    else if(41 <= pickCatch && pickCatch <= 55){
-      console.log("Black Striped Topminnow"); 
+    else if (48 <= pickCatch && pickCatch <= 62) {
+      river3Catch.style.width = 253 + "px";
+      river3Catch.style.height = 168 + "px";
+      river3Catch.style.backgroundImage = "url('blackstripeTopminnow.png')";
+      fishCaught = 28;
     }
-    else if(56 <= pickCatch && pickCatch <= 75){
-      console.log("Tadpole Madtom");
+    else if (63 <= pickCatch && pickCatch <= 77) {
+      river3Catch.style.width = 224.5 + "px";
+      river3Catch.style.height = 168.5 + "px";
+      river3Catch.style.backgroundImage = "url('tadpoleMadtom.png')";
+      fishCaught = 29;
     }
-    else{
-      console.log("Stonecat");
+    else {
+      river3Catch.style.width = 253 + "px";
+      river3Catch.style.height = 336 + "px";
+      river3Catch.style.backgroundImage = "url('stonecat.png')";
+      fishCaught = 30;
     }
   });
+}
+
+function infoAboutFish() {
+  switch (fishCaught) {
+    case 1:
+      document.getElementById("paintedhandMudbug").style.display = "block";
+      break;
+    case 2:
+      document.getElementById("calicoCrayfish").style.display = "block";
+      break;
+    case 3:
+      document.getElementById("bigWaterCrayfish").style.display = "block";
+      break;
+    case 4:
+      document.getElementById("fatheadMinnow").style.display = "block";
+      break;
+    case 5:
+      document.getElementById("centralStoneroller").style.display = "block";
+      break;
+    case 6:
+      document.getElementById("virileCrayfish").style.display = "block";
+      break;
+    case 7:
+      document.getElementById("riverChub").style.display = "block";
+      break;
+    case 8:
+      document.getElementById("goldenShiner").style.display = "block";
+      break;
+    case 9:
+      document.getElementById("northernClearwaterCrayfish").style.display = "block";
+      break;
+    case 10:
+      document.getElementById("commonShiner").style.display = "block";
+      break;
+    case 11:
+      document.getElementById("redsideDace").style.display = "block";
+      break;
+    case 12:
+      document.getElementById("northernPearlDace").style.display = "block";
+      break;
+    case 13:
+      document.getElementById("commonLogperch").style.display = "block";
+      break;
+    case 14:
+      document.getElementById("greensideDarter").style.display = "block";
+      break;
+    case 15:
+      document.getElementById("iowaDarter").style.display = "block";
+      break;
+    case 16:
+      document.getElementById("rainbowDarter").style.display = "block";
+      break;
+    case 17:
+      document.getElementById("johnnyDarter").style.display = "block";
+      break;
+    case 18:
+      document.getElementById("fantailDarter").style.display = "block";
+      break;
+    case 19:
+      document.getElementById("westernBlacknoseDace").style.display = "block";
+      break;
+    case 20:
+      document.getElementById("longnoseDace").style.display = "block";
+      break;
+    case 21:
+      document.getElementById("brindledMadtom").style.display = "block";
+      break;
+    case 22:
+      document.getElementById("mottledSculpin").style.display = "block";
+      break;
+    case 23:
+      document.getElementById("brookSilverside").style.display = "block";
+      break;
+    case 24:
+      document.getElementById("brookStickleback").style.display = "block";
+      break;
+    case 25:
+      document.getElementById("bandedKillifish").style.display = "block";
+      break;
+    case 26:
+      document.getElementById("centralMudminnow").style.display = "block";
+      break;
+    case 27:
+      document.getElementById("slimeySculpin").style.display = "block";
+      break;
+    case 28:
+      document.getElementById("blackstripeTopminnow").style.display = "block";
+      break;
+    case 29:
+      document.getElementById("tadpoleMadtom").style.display = "block";
+      break;
+    case 30:
+      document.getElementById("stonecat").style.display = "block";
+      break;
+  }
 }
 
 function swingNet1(currentRiverId, catchId) {
@@ -178,6 +345,20 @@ function swingNet1(currentRiverId, catchId) {
     dipnet1.classList.remove("animate");
     document.body.style.backgroundImage = "url('dipnettingBackground.png')";
   }, 1000);
+  river1Catch.style.display = "none";
+  numOfLeaves = 4; 
+  river1Leaf1.style.display = "block";
+  river2Leaf1.style.display = "block";
+  river3Leaf1.style.display = "block";
+  river1Leaf2.style.display = "block";
+  river2Leaf2.style.display = "block";
+  river3Leaf2.style.display = "block";
+  river1Leaf3.style.display = "block";
+  river2Leaf3.style.display = "block";
+  river3Leaf3.style.display = "block";
+  river1Leaf4.style.display = "block";
+  river2Leaf4.style.display = "block";
+  river3Leaf4.style.display = "block";
 }
 
 function swingNet2(currentRiverId, catchId) {
@@ -189,6 +370,20 @@ function swingNet2(currentRiverId, catchId) {
     dipnet2.classList.remove("animate");
     document.body.style.backgroundImage = "url('dipnettingBackground.png')";
   }, 1000);
+  river2Catch.style.display = "none";
+  numOfLeaves = 4; 
+  river1Leaf1.style.display = "block";
+  river2Leaf1.style.display = "block";
+  river3Leaf1.style.display = "block";
+  river1Leaf2.style.display = "block";
+  river2Leaf2.style.display = "block";
+  river3Leaf2.style.display = "block";
+  river1Leaf3.style.display = "block";
+  river2Leaf3.style.display = "block";
+  river3Leaf3.style.display = "block";
+  river1Leaf4.style.display = "block";
+  river2Leaf4.style.display = "block";
+  river3Leaf4.style.display = "block";
 }
 
 function swingNet3(currentRiverId, catchId) {
@@ -200,20 +395,34 @@ function swingNet3(currentRiverId, catchId) {
     dipnet3.classList.remove("animate");
     document.body.style.backgroundImage = "url('dipnettingBackground.png')";
   }, 1000);
+  river3Catch.style.display = "none";
+  numOfLeaves = 4; 
+  river1Leaf1.style.display = "block";
+  river2Leaf1.style.display = "block";
+  river3Leaf1.style.display = "block";
+  river1Leaf2.style.display = "block";
+  river2Leaf2.style.display = "block";
+  river3Leaf2.style.display = "block";
+  river1Leaf3.style.display = "block";
+  river2Leaf3.style.display = "block";
+  river3Leaf3.style.display = "block";
+  river1Leaf4.style.display = "block";
+  river2Leaf4.style.display = "block";
+  river3Leaf4.style.display = "block";
 }
 
 function hideLeaf1() {
   river1Leaf1.style.display = "none";
   river2Leaf1.style.display = "none";
   river3Leaf1.style.display = "none";
-  numOfLeaves -= 1; 
-  if(numOfLeaves == 0 && currentRiver == 1){
+  numOfLeaves -= 1;
+  if (numOfLeaves == 0 && currentRiver == 1) {
     river1Catch.style.display = "block";
   }
-  if(numOfLeaves == 0 && currentRiver == 2){
+  if (numOfLeaves == 0 && currentRiver == 2) {
     river2Catch.style.display = "block";
   }
-  if(numOfLeaves == 0 && currentRiver == 3){
+  if (numOfLeaves == 0 && currentRiver == 3) {
     river3Catch.style.display = "block";
   }
 }
@@ -221,14 +430,14 @@ function hideLeaf2() {
   river1Leaf2.style.display = "none";
   river2Leaf2.style.display = "none";
   river3Leaf2.style.display = "none";
-  numOfLeaves -= 1; 
-  if(numOfLeaves == 0 && currentRiver == 1){
+  numOfLeaves -= 1;
+  if (numOfLeaves == 0 && currentRiver == 1) {
     river1Catch.style.display = "block";
   }
-  if(numOfLeaves == 0 && currentRiver == 2){
+  if (numOfLeaves == 0 && currentRiver == 2) {
     river2Catch.style.display = "block";
   }
-  if(numOfLeaves == 0 && currentRiver == 3){
+  if (numOfLeaves == 0 && currentRiver == 3) {
     river3Catch.style.display = "block";
   }
 }
@@ -236,14 +445,14 @@ function hideLeaf3() {
   river1Leaf3.style.display = "none";
   river2Leaf3.style.display = "none";
   river3Leaf3.style.display = "none";
-  numOfLeaves -= 1; 
-  if(numOfLeaves == 0 && currentRiver == 1){
+  numOfLeaves -= 1;
+  if (numOfLeaves == 0 && currentRiver == 1) {
     river1Catch.style.display = "block";
   }
-  if(numOfLeaves == 0 && currentRiver == 2){
+  if (numOfLeaves == 0 && currentRiver == 2) {
     river2Catch.style.display = "block";
   }
-  if(numOfLeaves == 0 && currentRiver == 3){
+  if (numOfLeaves == 0 && currentRiver == 3) {
     river3Catch.style.display = "block";
   }
 }
@@ -251,16 +460,24 @@ function hideLeaf4() {
   river1Leaf4.style.display = "none";
   river2Leaf4.style.display = "none";
   river3Leaf4.style.display = "none";
-  numOfLeaves -= 1; 
-  if(numOfLeaves == 0 && currentRiver == 1){
+  numOfLeaves -= 1;
+  if (numOfLeaves == 0 && currentRiver == 1) {
     river1Catch.style.display = "block";
   }
-  if(numOfLeaves == 0 && currentRiver == 2){
+  if (numOfLeaves == 0 && currentRiver == 2) {
     river2Catch.style.display = "block";
   }
-  if(numOfLeaves == 0 && currentRiver == 3){
+  if (numOfLeaves == 0 && currentRiver == 3) {
     river3Catch.style.display = "block";
   }
+}
+
+function goBackToMain(hidden1, hidden){
+    document.getElementById(hidden1).style.display = 'none';
+    document.getElementById(hidden).style.display = 'none';
+    document.getElementById("main").style.display = 'block';
+    window.scrollTo(0, 0);
+    return false;
 }
 
 function isTouchDevice() {
